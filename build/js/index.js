cfbf36f22d669094d608aa3ff5d57f58f25ba458
d.fuchiwaki <d.fuchiwaki@everyleaf.com>
2013-10-17 00:22:36 +0900

    Change jQuery to Backbone

--- build/js/index.js
+++ build/js/index.js
@@ -1,11 +1,31 @@
 (function() {
-  jQuery(function() {
-    return $('#tab').on('click', '.tab', function(e) {
-      var $clickedTab;
-      $('#tab').find('.active').removeClass('active');
-      $clickedTab = $(e.target).addClass('active');
-      return $($clickedTab.attr('href')).addClass('active');
-    });
+  var TabView;
+
+  TabView = Backbone.View.extend({
+    el: "#tab",
+    events: {
+      "click .tab": "tabClick"
+    },
+    initialize: function() {
+      var _this = this;
+      return $(window).on("hashchange", function() {
+        return _this.activate(location.hash);
+      });
+    },
+    tabClick: function(e) {
+      return this.activate($(e.target).attr("href"));
+    },
+    activate: function(id) {
+      console.log(id);
+      this.$(".active").removeClass("active");
+      this.$("a[href=\"" + id + "\"]").addClass("active");
+      return this.$(id).addClass("active");
+    }
+  });
+
+  $(function() {
+    var view;
+    return view = new TabView();
   });
 
 }).call(this);
